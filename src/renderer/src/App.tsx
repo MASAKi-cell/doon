4cc4811c89c22b63afec2ed8c2b0cb9d8a949ab3
masaki takahashi <bje35149@gmail.com>
2024-03-24 23:47:33 +0900

    AD：コンポーネントの追加

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,24 +1,18 @@
+import { Root, Sidebar, Content } from './components/index'
+
 function App(): JSX.Element {
   const ipcHandle = (): Promise<void> => window.electron.sendPing()
 
   return (
     <>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
-      </div>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
+      <Root>
+        <Sidebar>Sidebar</Sidebar>
+        <Content>
+          {' '}
+          Build an Electron app with <span className="react">React</span>
+          &nbsp;and <span className="ts">TypeScript</span>
+        </Content>
+      </Root>
     </>
   )
 }
--- src/renderer/src/components/features/layout/AppLayout.tsx
+++ src/renderer/src/components/features/layout/AppLayout.tsx
@@ -1,10 +1,27 @@
-import styles from '@/styles/features/layout/AppLayout.module.scss'
-import { ComponentProps } from 'react'
+import { ComponentProps, forwardRef } from 'react'
+
+export const Root = ({ className, children, ...props }: ComponentProps<'main'>) => {
+  return (
+    <main className={className} {...props}>
+      {children}
+    </main>
+  )
+}
 
 export const Sidebar = ({ className, children, ...props }: ComponentProps<'aside'>) => {
   return (
-    <aside className={(styles.border, className)} {...props}>
-      {children}{' '}
+    <aside className={className} {...props}>
+      {children}
     </aside>
   )
 }
+
+export const Content = forwardRef<HTMLDivElement, ComponentProps<'div'>>(
+  ({ className, children, ...props }, ref) => {
+    return (
+      <div ref={ref} className={className} {...props}>
+        {children}
+      </div>
+    )
+  }
+)
