87c530b30551e31f7ec2e1427db6039c63e3b71e
masaki takahashi <bje35149@gmail.com>
2024-04-01 22:34:31 +0900

    ADD：スタイルをあてる箇所の修正

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,13 +1,14 @@
 import { Root, Sidebar, Content } from '@/renderer/src/components/index'
+import styles from '@/renderer/src/styles/pages/app.module.scss'
 
 function App(): JSX.Element {
   const ipcHandle = (): Promise<void> => window.electron.sendPing()
 
   return (
     <>
-      <Root>
-        <Sidebar>Sidebar</Sidebar>
-        <Content>
+      <Root className={styles.wrapper}>
+        <Sidebar className={styles.sidebar}>Sidebar</Sidebar>
+        <Content className={styles.content}>
           {' '}
           Build an Electron app with <span className="react">React</span>
           &nbsp;and <span className="ts">TypeScript</span>
--- src/renderer/src/components/features/layout/AppLayout.tsx
+++ src/renderer/src/components/features/layout/AppLayout.tsx
@@ -1,26 +1,17 @@
 import { ComponentProps, forwardRef } from 'react'
-import styles from '@/renderer/src/styles/features/layout/appLayout.module.scss'
 
-export const Root = ({ className, children, ...props }: ComponentProps<'main'>) => {
-  return (
-    <main className={(styles.wrapper, className)} {...props}>
-      {children}
-    </main>
-  )
+export const Root = ({ children, ...props }: ComponentProps<'main'>) => {
+  return <main {...props}>{children}</main>
 }
 
-export const Sidebar = ({ className, children, ...props }: ComponentProps<'aside'>) => {
-  return (
-    <aside className={(styles.sidebar, className)} {...props}>
-      {children}
-    </aside>
-  )
+export const Sidebar = ({ children, ...props }: ComponentProps<'aside'>) => {
+  return <aside {...props}>{children}</aside>
 }
 
 export const Content = forwardRef<HTMLDivElement, ComponentProps<'div'>>(
-  ({ className, children, ...props }, ref) => {
+  ({ children, ...props }, ref) => {
     return (
-      <div ref={ref} className={(styles.content, className)} {...props}>
+      <div ref={ref} {...props}>
         {children}
       </div>
     )
