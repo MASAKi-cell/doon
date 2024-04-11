f8305ed9a84fb59c1a861dda198e24b1400db335
masaki takahashi <bje35149@gmail.com>
2024-04-11 10:30:36 +0900

    ADD: svgの追加

--- src/renderer/src/components/features/button/DeleteNoteButton.tsx
+++ src/renderer/src/components/features/button/DeleteNoteButton.tsx
@@ -1,5 +1,11 @@
 import { ActionButton, ActionButtonProps } from './ActionButton'
+import style from '@/renderer/src/styles/features/button/deleteNoteButton.module.scss'
+import { FaRegTrashCan } from 'react-icons/fa6'
 
 export const DeleteNoteButton = ({ ...props }: ActionButtonProps) => {
-  return <ActionButton {...props}></ActionButton>
+  return (
+    <ActionButton {...props}>
+      <FaRegTrashCan className={style.wrapper} />
+    </ActionButton>
+  )
 }
--- src/renderer/src/components/features/button/NewNoteButton.tsx
+++ src/renderer/src/components/features/button/NewNoteButton.tsx
@@ -1,5 +1,11 @@
 import { ActionButton, ActionButtonProps } from './ActionButton'
+import style from '@/renderer/src/styles/features/button/newNoteButton.module.scss'
+import { LuFileSignature } from 'react-icons/lu'
 
 export const NewNoteButton = ({ ...props }: ActionButtonProps) => {
-  return <ActionButton {...props}></ActionButton>
+  return (
+    <ActionButton {...props}>
+      <LuFileSignature className={style.wrapper} />
+    </ActionButton>
+  )
 }
