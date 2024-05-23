e0d5b4229c9ddd56381d901e864012433370683c
masaki takahashi <bje35149@gmail.com>
2024-05-23 09:59:36 +0900

    ADD：atom処理の追加

--- src/renderer/src/store/useNotes.ts
+++ src/renderer/src/store/useNotes.ts
@@ -2,10 +2,10 @@ import { unwrap } from 'jotai/utils'
 import { atom } from 'jotai'
 
 /** tyes */
-import { NoteInfo } from '@renderer/contents/note'
+import { NoteContent, NoteInfo } from '@renderer/contents/note'
 
 export const useNotes = () => {
-  const selectedNoteIndexAtom = atom<number | null>(null)
+  const selectedNoteIndex = atom<number | null>(null)
 
   const getNotes = atom<Promise<NoteInfo[]>>(async () => {
     const notes = await window.electron.getNote()
@@ -14,15 +14,37 @@ export const useNotes = () => {
   })
   const notesAtom = unwrap(getNotes)
 
+  /** note選択 */
   const selectedNote = atom(async (get) => {
-    const slectedNoteIndex = get(selectedNoteIndexAtom)
+    const index = get(selectedNoteIndex)
     const notes = get(notesAtom)
 
-    if (!slectedNoteIndex || !notes) return
-    const selectedNote = notes[slectedNoteIndex]
+    if (!index || !notes) return
+    const selectedNote = notes[index]
 
     const noteContent = await window.electron.readNote(selectedNote.title)
+
+    return { ...selectedNote, content: noteContent }
   })
+
+  const selectedNoteAtom = unwrap(
+    selectedNote,
+    (prev) =>
+      prev ?? {
+        title: '',
+        content: '',
+        lastEditTime: Date.now()
+      }
+  )
+
+  /** noteの保存 */
+  const saveNote = atom(null, (get, set, newContent: NoteContent) => {})
+
+  /** noteの新規作成 */
+  const creatNote = atom(null, async (get, set) => {})
+
+  /** noteの削除 */
+  const deleteNote = atom(null, async (get, set) => {})
 
   return { selectedNoteIndexAtom, getNotes }
 }
