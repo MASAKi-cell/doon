bb2361e9602a35641b49afce173efe207823a2ac
masaki takahashi <bje35149@gmail.com>
2024-05-26 12:20:50 +0900

    FIX：型エラーの修正

--- src/renderer/src/store/useNotes.ts
+++ src/renderer/src/store/useNotes.ts
@@ -8,12 +8,12 @@ import { NoteContent, NoteInfo } from '@renderer/contents/note'
 export const useNotes = () => {
   const selectedNoteIndex = atom<number | null>(null)
 
-  const getNotes = atom<Promise<NoteInfo[]>>(async () => {
+  const getNotes = async () => {
     const notes = await window.electron.getNote()
     return notes.sort((a, b) => b.lastEditTime.getTime() - a.lastEditTime.getTime())
-  })
-
-  const notesAtom = unwrap(getNotes)
+  }
+  const notesAtomAsync = atom<NoteInfo[] | Promise<NoteInfo[]>>(getNotes())
+  const notesAtom = unwrap(notesAtomAsync)
 
   /** note選択 */
   const selectedNote = atom(async (get) => {
@@ -55,7 +55,7 @@ export const useNotes = () => {
 
     set(
       notesAtom,
-      notes.forEach((note) =>
+      notes.filter((note) =>
         note.title === selectedNote.title
           ? {
               ...note,
