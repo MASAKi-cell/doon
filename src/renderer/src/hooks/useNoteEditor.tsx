0a20b8ecaa19d0f23eff8a024d9b999aafd34a7e
masaki takahashi <bje35149@gmail.com>
2024-05-30 11:36:40 +0900

    AD D：rxjs

--- src/renderer/src/hooks/useNoteEditor.tsx
+++ src/renderer/src/hooks/useNoteEditor.tsx
@@ -1,10 +1,28 @@
 import { MDXEditorMethods } from '@mdxeditor/editor'
 import { useAtomValue, useSetAtom } from 'jotai'
 import { useRef } from 'react'
+
+/** store */
 import { useNotes } from '@renderer/store/index'
 
 export const useNoteEditor = () => {
   const { selectedNoteAtom, handleSaveNote } = useNotes()
   const selectedNote = useAtomValue(selectedNoteAtom)
   const saveNote = useSetAtom(handleSaveNote)
+  const editor = useRef<MDXEditorMethods>(null)
+
+  const handleBlur = async (): Promise<void> => {
+    if (!selectedNote) {
+      return
+    }
+
+    const content = editor.current?.getMarkdown() ?? ''
+
+    await saveNote(content)
+  }
+
+  return {
+    /** handle */
+    handleBlur
+  }
 }
