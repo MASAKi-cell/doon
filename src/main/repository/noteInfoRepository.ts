c97d995f638bc64367de69d0952dfd8835919996
masaki takahashi <bje35149@gmail.com>
2024-08-16 10:00:17 +0900

    ADD：createConnection

--- src/main/repository/noteInfoRepository.ts
+++ src/main/repository/noteInfoRepository.ts
@@ -1,11 +1,30 @@
 import Database from '@main/database/index'
-import { DataSource } from 'typeorm'
-
 import { NoteInfoModel } from '@main/database/model/noteInfo'
 
 /** types */
 import { NoteInfo } from '@main/contents/ipc'
 
+export const getNoteInfo = async () => {
+  const connection = await Database.createConnection()
+}
+
+export const readNoteInfo = async () => {
+  const connection = await Database.createConnection()
+}
+
+export const writeNoteInfo = async () => {
+  const connection = await Database.createConnection()
+}
+
 export const saveNoteInfo = async (noteInfo: NoteInfo) => {
-  ;(await Database.createConnection()).getRepository(NoteInfoModel).save(noteInfo)
+  const connection = await Database.createConnection()
+  connection.getRepository(NoteInfoModel).save(noteInfo)
+}
+
+export const createNoteInfo = async () => {
+  const connection = await Database.createConnection()
+}
+
+export const deleteNoteOInfo = async () => {
+  const connection = await Database.createConnection()
 }
