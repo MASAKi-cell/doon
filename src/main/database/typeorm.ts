d1ec7189da1391af5178f5fa89d32fa756638a3c
masaki takahashi <bje35149@gmail.com>
2024-09-02 09:55:11 +0900

    FIX：migrationsの設定修正

--- src/main/database/typeorm.ts
+++ src/main/database/typeorm.ts
@@ -2,14 +2,12 @@ import path from 'path'
 import { DataSourceOptions } from 'typeorm'
 import { NoteInfoModel } from '@main/database/model/noteInfo'
 
-console.log(path.join(__dirname, '/database/migrations'))
-
 export const ormconfig: DataSourceOptions = {
   type: 'sqlite',
-  database: __dirname + 'database/resources/database.sqlite',
+  database: path.join(__dirname, __dirname + '/database/resources/database.sqlite'),
   entities: [NoteInfoModel],
   migrations: [`${path.join(__dirname, '/database/migrations')}/*.js`],
   migrationsRun: true, // マイグレーション同時実行
-  synchronize: false,
+  synchronize: true,
   logging: true
 }
