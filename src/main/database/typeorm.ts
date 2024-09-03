8db7627e15444b9546bd2594d2ea0a6a553a22e9
masaki takahashi <bje35149@gmail.com>
2024-09-03 10:09:25 +0900

    FIX：pathの修正、typeormコマンドの追加

--- src/main/database/typeorm.ts
+++ src/main/database/typeorm.ts
@@ -1,12 +1,13 @@
 import path from 'path'
 import { DataSourceOptions } from 'typeorm'
 import { NoteInfoModel } from '@main/database/model/noteInfo'
+import { getHomeDir } from '@main/utils/index'
 
 export const ormconfig: DataSourceOptions = {
   type: 'sqlite',
-  database: path.join(__dirname, __dirname + '/database/resources/database.sqlite'),
+  database: path.join(getHomeDir(), 'src/main/database/resources/database.sqlite'),
   entities: [NoteInfoModel],
-  migrations: [`${path.join(__dirname, '/database/migrations')}/*.js`],
+  migrations: [path.join(getHomeDir(), 'src/main/database/migrations/*.js')],
   migrationsRun: true, // マイグレーション同時実行
   synchronize: true,
   logging: true
