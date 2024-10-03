a4bfb4237c60c7b169a1192a73b9aeb5bc0f874c
masaki takahashi <bje35149@gmail.com>
2024-10-03 19:39:10 +0900

    ADD：jotai DevToolsの導入

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -27,6 +27,12 @@ export default defineConfig({
         '@/mocks': resolve('src/renderer/src/mocks')
       }
     },
-    plugins: [react()]
+    plugins: [
+      react({
+        babel: {
+          presets: ['jotai/babel/preset']
+        }
+      })
+    ]
   }
 })
--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -10,26 +10,35 @@ import {
   NoteTitle
 } from '@renderer/components/index'
 
+/** jotai DevTools */
+import { createStore, Provider } from 'jotai'
+import { DevTools } from 'jotai-devtools'
+import 'jotai-devtools/styles.css'
+
 /** styles */
 import styles from '@renderer/styles/pages/app.module.scss'
 
 function App(): JSX.Element {
+  const customStore = createStore()
   return (
     <>
-      <TopBar />
-      <RootLayout className={styles.wrapper}>
-        {/* sidebar */}
-        <Sidebar className={styles.sidebar}>
-          <ActionButtonsRow className={styles.button} />
-          <NoteList />
-        </Sidebar>
+      <Provider store={customStore}>
+        <DevTools store={customStore} />
+        <TopBar />
+        <RootLayout className={styles.wrapper}>
+          {/* sidebar */}
+          <Sidebar className={styles.sidebar}>
+            <ActionButtonsRow className={styles.button} />
+            <NoteList />
+          </Sidebar>
 
-        {/* content */}
-        <Content className={styles.content}>
-          <NoteTitle />
-          <MarkdownEditor />
-        </Content>
-      </RootLayout>
+          {/* content */}
+          <Content className={styles.content}>
+            <NoteTitle />
+            <MarkdownEditor />
+          </Content>
+        </RootLayout>
+      </Provider>
     </>
   )
 }
